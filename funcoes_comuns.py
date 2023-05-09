35e02706f11f82e724d8d717923cf85e062551da
Josiney de Souza <josiney.souza@ifc.edu.br>
2023-05-09 19:07:41 -0300

    Adicionados comentários para as funções comuns
    Adicionados diversos comentários explicativos para as funções comuns que podem ser usadas pelo cliente, servidor e invasor

--- funcoes_comuns.py
+++ funcoes_comuns.py
@@ -1,3 +1,33 @@
+###############################################################################
+# Funcao cripto_subs()
+###############################################################################
+# Parametro: uma string (data) e um inteiro (chave)
+###############################################################################
+# Retorno: uma string (str_cripto_subs)
+###############################################################################
+#
+# A partir de uma string 'data', que representa os dados que serão
+# transmitidos, e de um inteiro 'chave', que representa uma chave publica ou
+# privada, faz um computo para criptografar a mensagem em 'data'
+#
+# Para criptografar, para cada caractere contido na string 'data' (o 'pos',
+# de posicao da string), se obtem seu correspondente em inteiro de acordo
+# com a tabela ASCII; depois se multiplica o valor obtido pela chave
+# (privada, por exemplo) e se guarda o modulo da divisao por 1000
+#
+# A princípio, pensou-se na tabela ASCII tradicional, de 7 bits (caracteres
+# de 0 a 127). Mesmo se for considerar caracteres acentuados graficamente
+# (uma tabela ASCII estendida, de 8 bits/1 byte, caracteres de 0 a 255),
+# cabem no limite do modulo de 1000
+#
+# O numero inteiro do caractere na tabela ASCII eh entao transformado em
+# caractere/string. Como serao enviados apenas numeros para o destino, para
+# se separar cada caractere, usa-se um espaco em branco; que eh retirado na
+# descriptografia.
+#
+# Por fim, retira-se o espaco em branco extra da ultima iteracao e retorna a
+# string criptografada
+###############################################################################
 def cripto_subs (data, chave):
     str_cripto_subs = ""
     for pos in data:
@@ -6,6 +36,36 @@ def cripto_subs (data, chave):
         str_cripto_subs = str_cripto_subs + str(codif) + " "
     return str_cripto_subs[:-1]
 
+
+
+###############################################################################
+# Funcao descripto_subs()
+###############################################################################
+# Parametro: uma string (data) e um inteiro (chave)
+###############################################################################
+# Retorno: uma string (str_descripto_subs)
+###############################################################################
+#
+# A partir de uma string 'data', que representa os dados que serão
+# transmitidos, e de um inteiro 'chave', que representa uma chave publica ou
+# privada, faz a descriptografia (o computo para descriptografar a mensagem
+# anteriormente criptografada com cripto_subs()
+#
+# Para descriptografar, como entre cada representacao inteira do caractere
+# (que esta transformado em caractere/string) ha um espaco em branco
+# inserido na criptografia, faz-se uma quebra da string a partir de espaços
+# em branco com a funcao split(). Isso resulta em uma lista que contem
+# apenas os numeros que representam as strings
+#
+# Assim, eh possivel iterar sobre a lista e, para cada numero representado
+# como um caractere/string, transforma-o novamente em inteiro. Apos, se
+# se multiplica o valor obtido pela chave (publica, por exemplo) e se
+# guarda o modulo da divisao por 1000 (inverso multiplicativo modular)
+#
+# A partir desse numero, que agora eh um inteiro que representa o caractere
+# descriptografado, transforma-o em caractere e se reconstroi a string
+# original e a retorna
+###############################################################################
 def descripto_subs (data, chave):
     str_descripto_subs = ""
     for num in data.split():
