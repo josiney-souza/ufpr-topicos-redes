dd71579c970453b13b79dc5a7e57793476931e69
Josiney de Souza <josiney.souza@ifc.edu.br>
2023-05-12 17:21:36 -0300

    Códigos para adicionar TLS no cliente e no servidor
    Foram adicionados códigos no servidor e no cliente para que se comuniquem com conexões encapsuladas seguras usando TLS

--- 03-cliente.py
+++ 03-cliente.py
@@ -12,6 +12,8 @@
 
 # Adicao do modulo socket para conseguir realizar a conexao cliente-servidor
 import socket
+# Adicao do modulo ssl para usar o TLS e tornar a comunicacao segura
+import ssl
 # Adicao de modulo de funcoes e configuracoes comuns ao cliente e servidor
 import confs_comuns
 
@@ -25,27 +27,33 @@ import confs_comuns
 # SOCK_STREAM: para se usar TCP
 socket_s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+contexto = ssl.create_default_context()
+contexto.check_hostname = False
+contexto.verify_mode = ssl.CERT_NONE
+#contexto = ssl._create_unverified_context()
+socket_tls = contexto.wrap_socket(socket_s, server_hostname="josiney")
+
 # Usa o socket para se conectar ao servidor
-socket_s.connect((confs_comuns.END_SERVIDOR, confs_comuns.PORTA))
+socket_tls.connect((confs_comuns.END_SERVIDOR, confs_comuns.PORTA))
 
 # Quando conectado, envia uma string ao servidor. Para enviar, eh necessario
 # codifica-la como bytes, por isso se usa a funcao "encode()"
 print("Enviando mensagem pré-definida ao servidor")
-socket_s.send("Olá, servidor!".encode())
+socket_tls.send("Olá, servidor!".encode())
 
 # Aguarda pela mensagem de confirmação do servidor e a imprime. Como ela vem
 # codificada como bytes, eh necessario decodifica-la com a funcao "decode()"
-dados = socket_s.recv(1024).decode()
+dados = socket_tls.recv(1024).decode()
 print("Recebido do servidor:", dados)
 
 # Obtem uma mensagem digitada pelo usuario e a envia ao servidor
 dados = "O que quer enviar?\n" + confs_comuns.PROMPT[1] + " "
 dados = input(dados)
-socket_s.send(dados.encode())
+socket_tls.send(dados.encode())
 
 # Aguarda pela mensagem de confirmação do servidor e a imprime
-dados = socket_s.recv(1024).decode()
+dados = socket_tls.recv(1024).decode()
 print("Recebido do servidor:", dados)
 
 # Encerra a conexao com o servidor e finaliza
-socket_s.close()
+socket_tls.close()
--- 03-servidor.py
+++ 03-servidor.py
@@ -12,6 +12,8 @@
 
 # Adicao do modulo socket para conseguir realizar a conexao cliente-servidor
 import socket
+# Adicao do modulo ssl para usar o TLS e tornar a comunicacao segura
+import ssl
 # Adicao de modulo de funcoes e configuracoes comuns ao cliente e servidor
 import confs_comuns
 
@@ -24,6 +26,8 @@ import confs_comuns
 # AF_INET: para se usar IPv4
 # SOCK_STREAM: para se usar TCP
 socket_s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+contexto = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
+contexto.load_cert_chain(certfile="certificado.crt", keyfile="chave-privada.pem")
 
 # Associa o endereco do servidor e a porta de uso com o socket
 socket_s.bind((confs_comuns.END_SERVIDOR, confs_comuns.PORTA))
@@ -33,6 +37,7 @@ socket_s.listen()
 
 # Depois de estar em modo de escuta, aceita as conexoes de clientes
 conexao, end_cliente = socket_s.accept()
+constream = contexto.wrap_socket(conexao, server_side=True)
 
 # Mostra que um cliente se conectou e exibe seu endereco e porta contidos
 # na variavel "end_cliente"
@@ -40,27 +45,27 @@ print(confs_comuns.MSG["clientecon"], end_cliente)
 
 # Recebe os dados enviados pelo cliente. Como ela vem codificada como
 # bytes, eh necessario decodifica-la com a funcao "decode()"
-dados = conexao.recv(1024).decode()
+dados = constream.recv(1024).decode()
 
 # Exibe a mensagem recebida do cliente
 print(confs_comuns.MSG["docliente"], dados)
 
 # Envia mensagem de confirmação para o cliente
 dados = confs_comuns.RETORNOS["ack"]
-conexao.send(dados.encode())
+constream.send(dados.encode())
 
 # Aguarda uma nova mensagem do cliente
-dados = conexao.recv(1024).decode()
+dados = constream.recv(1024).decode()
 
 # Exibe a nova mensagem
 print(confs_comuns.MSG["docliente"], dados)
 
 # Envia mais uma confirmacao de recebimento da mensagem
 dados = confs_comuns.RETORNOS["ack"]
-conexao.send(dados.encode())
+constream.send(dados.encode())
 
 # Recebe a mensagem de fechamento da conexao do cliente e finaliza
-dados = conexao.recv(1024).decode()
+dados = constream.recv(1024).decode()
 print(confs_comuns.MSG["fim"])
 
 # Encerra a conexao com o cliente
